6ca4bb594fbfb0034b93a6775f7ba6f4c56b2f5a
harryph1ll1ps <harry.t.phillips1@gmail.com>
2026-03-25 14:47:42 +1100

    Register IPC handlers during bootstrap

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,11 @@
 import { startApp } from './app';
+import { registerIpcHandlers } from './ipc';
 import { registerGlobalShortcuts } from './shortcuts';
 import { createOverlayWindow } from './windows/overlay-window';
 
 async function bootstrap(): Promise<void> {
 	await startApp();
+	registerIpcHandlers();
 
 	registerGlobalShortcuts({
 		startSnipFlow: createOverlayWindow
